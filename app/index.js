b495cd5ddc7bf32308d5f2cfe6eaed2cd7e2a6ee
karthikkumar1996 <jkkr.1996@gmail.com>
2018-12-18 17:58:19 +0530

    initial commit

---
+++ app/index.js
@@ -0,0 +1,104 @@
+/*
+ *  Primary file for the API
+ *
+*/
+
+//Dependencies
+var http = require('http');
+var https = require('https');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./lib/config');
+var fs = require('fs');
+var handlers = require('./lib/handlers')
+var helpers = require('./lib/helpers');
+
+// Core server logic to be shared between the http and https servers
+var unifiedServerCoreLogic = function (req, res) {
+
+    var payloadBuffer = '';
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path 
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an Object
+    var queryStringObject = parsedUrl.query;
+
+    // Get the HTTP method
+    var method = req.method.toLowerCase();
+
+    // Get the Headers as an object
+    var headers = req.headers;
+
+    // Get the Payload, if any
+    var decoder = new StringDecoder('utf-8');
+
+    req.on('data', function (data) {
+        payloadBuffer += decoder.write(data);
+    });
+
+    req.on('end', function () {
+        payloadBuffer += decoder.end();
+
+        // Choose the handler this request should go to. If one is not found, use the not found handler
+        var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        //construct the data object to send to the handler
+        var data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': helpers.parseJsonToObject(payloadBuffer)
+        };
+
+        // Simplified way for chosen handler
+        chosenHandler(data, function (statusCode, payload) {
+            // Use the status code called back by the handler or default to 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            // Use the paylaod called back by the handler or default to an empty object
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            // Convert payload to a string
+            var payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+
+            // Send the response    
+            res.end(payloadString);
+
+            // Log the response
+            console.log("Returning the response: ", statusCode, payloadString)
+        });
+
+    });
+}
+
+// Instantiating the https server
+var httpServer = http.createServer(function (req, res) {
+    unifiedServerCoreLogic(req, res);
+});
+
+// Instantiating the https server
+var httpsServerOptions = {
+    // 'key': fs.readFileSync('./https/key.pem'),
+    // 'cert': fs.readFileSync('./https/cert.perm')
+};
+
+//Start the http server and have it listen on port 3000
+httpServer.listen(config.httpPort, function () {
+    console.log("The server is listening on port " + config.httpPort);
+});
+
+// Define a request router
+
+var router = {
+    'ping': handlers.ping,
+    'text':handlers.text
+}
